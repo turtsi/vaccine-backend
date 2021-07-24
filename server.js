ba303069713e375c90afd404ce9afa996c389adf
Mikko Turunen <mikko.turunen89@gmail.com>
2021-07-24 09:48:54 +0300

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const antiquaRoute = require("./routes/antiquaRoute");
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/antiqua", antiquaRoute);
