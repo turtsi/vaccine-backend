7353b404acd1f07cc875974d8af23b7e2414e091
Mikko Turunen <mikko.turunen89@gmail.com>
2021-07-31 09:04:25 +0300

    testing

--- routes/vaccineRoute.js
+++ routes/vaccineRoute.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Antiqua = require("../models/Antiqua");
+const Zerpfy = require("../models/Zerpfy");
+const Solar = require("../models/SolarBuddhica");
+const Vaccines = require("../models/Vaccinations");
 
 const { getData } = require("../controller/vaccineControllers");
 
 // router.get("/", getData);
 
-router.get("/", async (req, res) => {
+router.get("/vaccines", async (req, res) => {
+  let string = req.query.healthCareDistrict;
+  console.log(string);
+
   try {
-    const data = await Antiqua.find(req.query.rp);
+    const data = await Antiqua.find({ healthCareDistrict: string });
+    db.Antiqua.find({});
+    console.log(data);
+    // const data = await Antiqua.find(req.query.rp);
     res.json(data);
   } catch (error) {
     console.error(error);
@@ -16,4 +25,14 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/vaccines/date", async (req, res) => {
+  let date = req.query.d;
+  try {
+    const data = await Antiqua.find({ arrived: date });
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 module.exports = router;
