a37ef7dc9b7b2b95b97f100c7f3c14de018b94b9
Mikko Turunen <mikko.turunen89@gmail.com>
2021-08-04 11:55:05 +0300

    added GET functionalities

--- routes/vaccineRoute.js
+++ routes/vaccineRoute.js
@@ -1,38 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const Antiqua = require("../models/Antiqua");
-const Zerpfy = require("../models/Zerpfy");
-const Solar = require("../models/SolarBuddhica");
-const Vaccines = require("../models/Vaccinations");
-
-const { getData } = require("../controller/vaccineControllers");
-
-// router.get("/", getData);
+const dayjs = require("dayjs");
+const {
+  getAntiqua,
+  getZerpfy,
+  getSolar,
+} = require("../controllers/vaccineControllers");
 
 router.get("/vaccines", async (req, res) => {
-  let string = req.query.healthCareDistrict;
-  console.log(string);
+  if (req.query.d != null) {
+    if (!validateDate(req.query.d)) res.json({ message: "Invalid date" });
+    try {
+      const date = dayjs(req.query.d).format("YYYY-MM-DDTHH:mm:ss.SSSSSS[Z]");
 
-  try {
-    const data = await Antiqua.find({ healthCareDistrict: string });
-    db.Antiqua.find({});
-    console.log(data);
-    // const data = await Antiqua.find(req.query.rp);
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
+      if (req.query.v === "antiqua") {
+        const data = await getAntiqua(date);
+        res.json(data);
+      } else if (req.query.v === "zerpfy") {
+        const data = await getZerpfy(date);
+        res.json(data);
+      } else if (req.query.v === "solar") {
+        const data = await getSolar(date);
+        res.json(data);
+      } else {
+        res.json({ message: "Invalid query" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  } else {
+    res.json({ message: "Invalid query" });
   }
 });
 
-router.get("/vaccines/date", async (req, res) => {
-  let date = req.query.d;
-  try {
-    const data = await Antiqua.find({ arrived: date });
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
-});
+const validateDate = (date) => {
+  const format = "YYYY-MM-DD";
+  return dayjs(date, format).format(format) === date;
+};
+
 module.exports = router;
